ab574c42594ef4b2d1f29849fa82e61b51174f5e
Jonathan Koch <john.koch.pro@gmail.com>
2020-12-22 21:44:41 +0100

    next

---
+++ Test/src/com/sundaday/jeu/Main.java
@@ -0,0 +1,14 @@
+package com.sundaday.jeu;
+
+import java.awt.*;
+
+public class Main {
+
+    private static Object Rectangle;
+
+    public static void main(String[] args) {
+        Rectangle rect = null;
+        rect.width = 40;
+        rect.height = 50;
+        System.out.println("Le rectangle a une aire de " + rect.area(Rectangle));    }
+}
